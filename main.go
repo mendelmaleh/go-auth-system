b8d85d284507f757ef6c5d43c119dde3679db5ef
Mendel E <mendelmaleh@gmail.com>
2020-09-03 07:52:05 -0400

    Refactor
    - use log.Println instead of fmt.Println
    - use io.WriteString instead of fmt.Fprint[f]
    - don't use global vars
    - use const for pinDuration
    - other changes

--- main.go
+++ main.go
@@ -1,65 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	redis "github.com/alphazero/Go-Redis"
 )
 
-var spec *redis.ConnectionSpec
-var client redis.Client
-var err error
-
-var pinDuration int64 = 60 // How many time the pin should be valid (in seconds)
+const pinDuration int64 = 60 // how long the pin should be valid (in seconds)
 
 func main() {
-	spec = redis.DefaultSpec().Db(2)
-	client, err = redis.NewSynchClientWithSpec(spec)
+	spec := redis.DefaultSpec().Db(2)
+	client, err := redis.NewSynchClientWithSpec(spec)
 	if err != nil {
-		fmt.Println("Failed to create the client", err)
+		log.Println("failed to create the client", err)
 		return
 	}
 
-	http.HandleFunc("/checkPin", checkPin)
-	http.HandleFunc("/getPin", getPin)
+	http.HandleFunc("/getPin/", func(w http.ResponseWriter, r *http.Request) {
+		out, err := getPin(client)
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(w, out)
+	})
+
+	http.HandleFunc("/checkPin/", func(w http.ResponseWriter, r *http.Request) {
+		out, err := checkPin(client, r.URL.Query())
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(w, out)
+	})
 
 	http.ListenAndServe(":80", nil)
 }
 
+func getPin(client redis.Client) (string, error) {
+	pin := strconv.Itoa(rangeIn(100000, 999999))
+
+	if err := client.Set(pin, []byte("active")); err != nil {
+		return "error: failed setting pin in the database", err
+	}
+
+	if _, err := client.Expire(pin, pinDuration); err != nil {
+		return "error: failed setting expiratoin in the database", err
+	}
+
+	return pin, nil
+}
+
+func checkPin(client redis.Client, values url.Values) (string, error) {
+	pin, ok := values["pin"]
+	if !ok || len(pin[0]) < 1 {
+		return "error: pin is missing", nil
+	}
+
+	exists, err := client.Exists(pin[0])
+	if err != nil {
+		return "error: failed getting pin from database", err
+	}
+
+	return strconv.FormatBool(exists), nil
+}
+
 func rangeIn(low, hi int) int {
 	return low + rand.Intn(hi-low)
-}
-
-func checkPin(w http.ResponseWriter, req *http.Request) {
-	pin, ok := req.URL.Query()["pin"]
-	if !ok || len(pin[0]) < 1 {
-		fmt.Fprintf(w, "Error: pin is missing.")
-		return
-	}
-
-	ex, err := client.Exists(pin[0])
-	if err != nil {
-		fmt.Fprintf(w, "Error: failed getting pin from DB.")
-		return
-	}
-
-	if ex {
-		fmt.Fprintf(w, "true")
-	} else {
-		fmt.Fprintf(w, "false")
-	}
-}
-
-func getPin(w http.ResponseWriter, req *http.Request) {
-	pin := strconv.Itoa(rangeIn(100000, 999999))
-	err := client.Set(pin, []byte("active"))
-	client.Expire(pin, pinDuration)
-	if err != nil {
-		fmt.Fprintf(w, "Error: failed setting pin in the DB.")
-		return
-	}
-	fmt.Fprintf(w, pin)
 }
